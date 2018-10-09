24205a34259b8a1044a5610ab1d14a850f9dbc74
Christian Nicolaisen <cvn_vitting@hotmail.com>
2018-10-09 22:15:28 +0200

    added customization

--- lib/helpers/chip_header.dart
+++ lib/helpers/chip_header.dart
@@ -5,22 +5,39 @@ class ChipHeader extends StatelessWidget {
   final double fontSize;
   final TextAlign textAlign;
   final FontWeight fontWeight;
+  final Color backgroundColor;
+  final Color color;
+  final bool expanded;
+  final bool roundedCorners;
 
   const ChipHeader(this.text,
       {this.fontSize = 14.0,
       this.fontWeight = FontWeight.normal,
-      this.textAlign = TextAlign.start});
+      this.textAlign = TextAlign.start,
+      this.color = Colors.white,
+      this.backgroundColor = Colors.blue,
+      this.roundedCorners = true,
+      this.expanded = false});
   @override
   Widget build(BuildContext context) {
+    Widget widgets = Text(text,
+        textAlign: textAlign,
+        style: TextStyle(
+            fontSize: fontSize, color: color, fontWeight: fontWeight));
+
+    if (expanded) {
+      widgets = Expanded(child: widgets);
+    }
+
     return Container(
         padding: EdgeInsets.all(5.0),
         decoration: BoxDecoration(
-            color: Colors.blue, borderRadius: BorderRadius.circular(6.0)),
-        child: Text(text,
-            textAlign: textAlign,
-            style: TextStyle(
-                fontSize: fontSize,
-                color: Colors.white,
-                fontWeight: fontWeight)));
+            color: backgroundColor, borderRadius: roundedCorners ? BorderRadius.circular(6.0) : null),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            widgets
+          ],
+        ));
   }
 }
