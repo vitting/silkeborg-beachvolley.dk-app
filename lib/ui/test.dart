9b5740c08b23272e3ebe896632c61da559269565
Christian Nicolaisen <cvn_vitting@hotmail.com>
2018-10-18 15:54:07 +0200

    test

--- lib/ui/test.dart
+++ lib/ui/test.dart
@@ -1,9 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:silkeborgbeachvolley/helpers/notification_categories_enum.dart';
-import 'package:silkeborgbeachvolley/helpers/user_messaging_data.dart';
+import 'package:silkeborgbeachvolley/helpers/firebase_functions_call.dart';
 import 'package:silkeborgbeachvolley/ui/scaffold/SilkeborgBeachvolleyScaffold.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 
 class TestWidget extends StatefulWidget {
   @override
@@ -11,39 +9,7 @@ class TestWidget extends StatefulWidget {
 }
 
 class _TestWidgetState extends State<TestWidget> {
-  final FirebaseMessaging _firebaseMessaging = new FirebaseMessaging();
-
-  @override
-    void initState() {
-      super.initState();
-      _firebaseMessaging.requestNotificationPermissions(IosNotificationSettings(
-        alert: true,
-        badge: true,
-        sound: true
-      ));
-      _firebaseMessaging.configure(
-        ///OnLaunch er der ikke body og titel med. Bliver executed når appen er termineret
-        onLaunch: (Map<String, dynamic> message) {
-          print("ONLAUNCH: $message");
-        },
-        ///OnMessage er der body og titel med. Bliver executed når appen er aktiv
-        onMessage: (Map<String, dynamic> message) {
-          print("ONMESSAGE: $message");
-        },
-        ///OnResume er der ikke body og titel med. Bliver executed når appen er minimeret
-        onResume: (Map<String, dynamic> message) {
-          print("ONRESUME: $message");
-        }
-      );
-
-     _firebaseMessaging.onTokenRefresh.listen((token) {
-       print("TOKEN REFRESH: $token");
-     });
-
-      _firebaseMessaging.getToken().then((String token) {
-        print(token);
-      });
-    }
+  
   @override
   Widget build(BuildContext context) {
     return SilkeborgBeachvolleyScaffold(
@@ -53,9 +19,8 @@ class _TestWidgetState extends State<TestWidget> {
             children: <Widget>[
               RaisedButton(
                 onPressed: () async {
-                  Firestore firestore = Firestore.instance;
-                  QuerySnapshot snapshot = await firestore.collection("users_messaging").where("subscriptions", arrayContains: "palle").getDocuments();
-                  print("QUERY: ${snapshot.documents.length}");
+                  String result = await FirebaseFunctions.resetRanking();
+                  print("FUNCTIONS: $result");
                 },
               )
             ],
