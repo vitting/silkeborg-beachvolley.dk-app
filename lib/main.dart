37a0082fd493de32838c382b13c97dd8b2df9ec5
Christian Nicolaisen <cvn_vitting@hotmail.com>
2018-10-21 16:10:44 +0200

    added localization for english, only Google translate for now

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_i18n/flutter_i18n_delegate.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:silkeborgbeachvolley/ui/bulletin/main/bulletin_main.dart';
-import 'package:silkeborgbeachvolley/ui/enrollment/admin/admin_enrollment_main.dart';
-import 'package:silkeborgbeachvolley/ui/enrollment/main/enrollment_main.dart';
-import 'package:silkeborgbeachvolley/ui/home/home_main.dart';
-import 'package:silkeborgbeachvolley/ui/login/login_main.dart';
-import 'package:silkeborgbeachvolley/ui/ranking/admin/admin_ranking_main.dart';
-import 'package:silkeborgbeachvolley/ui/ranking/main/ranking_main.dart';
-import 'package:silkeborgbeachvolley/ui/scoreboard/scoreboard_main.dart';
-import 'package:silkeborgbeachvolley/ui/settings/settings_main.dart';
-import 'package:silkeborgbeachvolley/ui/test.dart';
-import 'package:silkeborgbeachvolley/ui/tournament_calendar/main/tournament_calendar_main.dart';
+import 'package:silkeborgbeachvolley/main_material_app.dart';
 
 void main() async {
   /// Set firestore to save and return dates as Timestamp
@@ -21,32 +9,5 @@ void main() async {
   final Firestore firestore = Firestore(app: app);
   await firestore.settings(timestampsInSnapshotsEnabled: true);
 
-  runApp(MaterialApp(
-    title: 'Silkeborg Beachvolley',
-    locale: const Locale("da"),
-    supportedLocales: [const Locale("da")],
-    localizationsDelegates: [
-      FlutterI18nDelegate(false, "da"),
-      GlobalMaterialLocalizations.delegate,
-      GlobalWidgetsLocalizations.delegate
-    ],
-    theme: ThemeData(
-        // primaryColor: Colors.yellow
-        ),
-    initialRoute: "/",
-    routes: <String, WidgetBuilder>{
-      "/": (BuildContext context) => Home(),
-      // "/": (BuildContext context) => TestWidget(),
-      Enrollment.routeName: (BuildContext context) => Enrollment(),
-      AdminEnrollment.routeName: (BuildContext context) => AdminEnrollment(),
-      ScoreBoard.routeName: (BuildContext context) => ScoreBoard(),
-      Bulletin.routeName: (BuildContext context) => Bulletin(),
-      Login.routeName: (BuildContext context) => Login(),
-      Settings.routeName: (BuildContext context) => Settings(),
-      Ranking.routeName: (BuildContext context) => Ranking(),
-      AdminRanking.routeName: (BuildContext context) => AdminRanking(),
-      TournamentCalendar.routeName: (BuildContext context) =>
-          TournamentCalendar()
-    },
-  ));
+  runApp(MainMaterialApp());
 }
---
+++ lib/main_material_app.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n_delegate.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:silkeborgbeachvolley/ui/bulletin/main/bulletin_main.dart';
+import 'package:silkeborgbeachvolley/ui/enrollment/admin/admin_enrollment_main.dart';
+import 'package:silkeborgbeachvolley/ui/enrollment/main/enrollment_main.dart';
+import 'package:silkeborgbeachvolley/ui/home/home_main.dart';
+import 'package:silkeborgbeachvolley/ui/login/login_main.dart';
+import 'package:silkeborgbeachvolley/ui/ranking/admin/admin_ranking_main.dart';
+import 'package:silkeborgbeachvolley/ui/ranking/main/ranking_main.dart';
+import 'package:silkeborgbeachvolley/ui/scoreboard/scoreboard_main.dart';
+import 'package:silkeborgbeachvolley/ui/settings/settings_main.dart';
+import 'package:silkeborgbeachvolley/ui/tournament_calendar/main/tournament_calendar_main.dart';
+
+class MainMaterialApp extends StatefulWidget {
+  @override
+  _MainMaterialAppState createState() => _MainMaterialAppState();
+}
+
+class _MainMaterialAppState extends State<MainMaterialApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Silkeborg Beachvolley",
+      localizationsDelegates: [
+        FlutterI18nDelegate(false, "en"),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      theme: ThemeData(
+          // primaryColor: Colors.yellow
+          ),
+      initialRoute: "/",
+      routes: <String, WidgetBuilder>{
+        "/": (BuildContext context) => Home(),
+        // "/": (BuildContext context) => TestWidget(),
+        Enrollment.routeName: (BuildContext context) => Enrollment(),
+        AdminEnrollment.routeName: (BuildContext context) => AdminEnrollment(),
+        ScoreBoard.routeName: (BuildContext context) => ScoreBoard(),
+        Bulletin.routeName: (BuildContext context) => Bulletin(),
+        Login.routeName: (BuildContext context) => Login(),
+        Settings.routeName: (BuildContext context) => Settings(),
+        Ranking.routeName: (BuildContext context) => Ranking(),
+        AdminRanking.routeName: (BuildContext context) => AdminRanking(),
+        TournamentCalendar.routeName: (BuildContext context) =>
+            TournamentCalendar()
+      },
+    );
+  }
+}
