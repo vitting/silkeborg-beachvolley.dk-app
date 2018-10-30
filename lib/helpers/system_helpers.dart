27c673e767fd8090d4777d2a12a485a21b4058f6
Christian Nicolaisen <cvn_vitting@hotmail.com>
2018-10-30 20:48:24 +0100

    added keep screen on function

--- lib/helpers/system_helpers.dart
+++ lib/helpers/system_helpers.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:screen/screen.dart';
 
 class SystemHelpers {
   static void hideKeyboardWithNoFocus(BuildContext context) {
@@ -10,5 +11,20 @@ class SystemHelpers {
 
   static Future<dynamic> hideKeyboardWithFocus() {
     return SystemChannels.textInput.invokeMethod('TextInput.hide');
+  }
+
+  static Future<void> setScreenOn() async {
+    bool isScreenOn = await Screen.isKeptOn;
+    if (isScreenOn == false) {
+      Screen.keepOn(true);
+    }
+  }
+
+  static Future<void> setScreenOff() async {
+    bool isScreenOn = await Screen.isKeptOn;
+
+    if (isScreenOn == true) {
+      Screen.keepOn(false);
+    }
   }
 }
