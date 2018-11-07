6b22a329782ae415c3c58b65edd25b9e969da412
Christian Nicolaisen <cvn_vitting@hotmail.com>
2018-11-07 11:21:45 +0100

    removed getConfig()

--- lib/helpers/system_helpers.dart
+++ lib/helpers/system_helpers.dart
@@ -1,10 +1,7 @@
 import 'dart:async';
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:screen/screen.dart';
-import 'package:silkeborgbeachvolley/helpers/config_data.dart';
-import 'package:silkeborgbeachvolley/main_inheretedwidget.dart';
 
 class SystemHelpers {
   static void hideKeyboardWithNoFocus(BuildContext context) {
@@ -37,50 +34,5 @@ class SystemHelpers {
     } else {
       return SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
     }
-  }
-
-  // static Future<Map<String, dynamic>> getConfig(BuildContext context) async {
-    
-  //   Map<String, dynamic> config;
-  //   try {
-  //     String data = "";
-  //     switch (MainInherited.of(context).modeProfile) {
-  //       case SystemMode.develop:
-  //         data = await DefaultAssetBundle.of(context).loadString("assets/files/config_develop.json");          
-  //         break;
-  //       case SystemMode.release:
-  //         data = await DefaultAssetBundle.of(context).loadString("assets/files/config_release.json");          
-  //         break;
-  //     }
-
-  //     config = json.decode(data);
-  //   } catch (e) {
-  //     print(e);
-  //   }
-
-  //   return config;
-  // }
-
-  static Future<ConfigData> getConfig(BuildContext context) async {
-    ConfigData config;
-    
-    try {
-      String data = "";
-      switch (MainInherited.of(context).modeProfile) {
-        case SystemMode.develop:
-          data = await DefaultAssetBundle.of(context).loadString("assets/files/config_develop.json");          
-          break;
-        case SystemMode.release:
-          data = await DefaultAssetBundle.of(context).loadString("assets/files/config_release.json");          
-          break;
-      }
-
-      Map<String, dynamic> jsonDecoded = json.decode(data);
-      config = ConfigData.fromMap(jsonDecoded);
-    } catch (e) {
-      print(e);
-    }
-
-    return config;
   }
 }
